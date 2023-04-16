c52d1defd9ed499e01f68f47806af13da876a0b1
juanalsan <118403734+juanalsan@users.noreply.github.com>
2023-04-16 23:01:35 +0200

    Update and rename broker.py to ej1_broker.py

--- ej1_broker.py
+++ ej1_broker.py
@@ -2,16 +2,15 @@ from paho.mqtt.client import Client
 
 def on_message(client, userdata, msg):
     print(msg.topic, msg.payload)
+    print("MESSAGE: ", msg.topic, msg.payload)
+    client.publish('clients/topic_test', msg.payload)
 
 def main(broker, topic):
     client = Client()
     client.on_message = on_message
-
     print(f'Connecting on channels {topic} on {broker}')
     client.connect(broker)
-
     client.subscribe(topic)
-
     client.loop_forever()
 
 if __name__ == "__main__":
